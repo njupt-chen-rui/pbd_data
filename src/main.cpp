46314f672330603aef5cc39528c3986fa7a0469a
njupt-chen-rui <527810883@qq.com>
2022-12-22 10:27:07 +0800

    1222

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<pbd_data/datastruct.h>
 #include<pbd_data/readinp.h>
+#include<pbd_data/tetEdgeIds.h>
 using namespace std;
 
 int main(){
@@ -13,6 +14,10 @@ int main(){
     vector<vert> verts;
     vector<tet> tets;
     readinp_tet(input_path, verts, tets);
+    vector<int> tet_edge_ids;
+    tetEdgeIds(verts, tets, tet_edge_ids);
+    tetSurfaceTriIds();
+    // get_tet_edge();
     // cout<<verts.size()<<endl;
     // for(size_t i = 0;i<10;i++){
     //     cout<<verts[i].x<<" "<<verts[i].y<<" "<<verts[i].z<<endl;
@@ -22,7 +27,7 @@ int main(){
     //     cout<<tets[i].v[0]<<" "<<tets[i].v[1]<<" "<<tets[i].v[2]<<" "<<tets[i].v[3]<<endl;
     // }
 
-    
+
     // output_meshdata(output_path, file_name);
     return 0;
 }
